4acb5c5874552bc3634d946e3632b3457ed613c0
safdarbhatti2-glitch <safdar@rushparcel.co.uk>
2026-09-04 16:06:26 +0500

    Fix unclosed style tag in layouts/admin.php that was causing browser blank white page on admin panel route

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -177,6 +177,7 @@
             .tablepanel { overflow: auto; }
             .table { min-width: 650px; }
         }
+    </style>
     <!-- Favicons & Brand Icons -->
     <link rel="icon" type="image/x-icon" href="<?= asset('favicon.ico') ?>">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= asset('brand/favicon-32x32.png') ?>">
